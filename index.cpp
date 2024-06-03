16f2920a27dcd7501ae57005ad161a5d87887d84
mamun2519 <juborajislam46@gmail.com>
2024-06-03 22:01:03 +0600

    complate

--- index.cpp
+++ index.cpp
@@ -2,12 +2,7 @@
 #include <vector>
 using namespace std;
 void runingSum(vector<int> &v)
-{
-      for (int i = 1; i < v.size(); i++)
-      {
-            v[i] += v[i - 1];
-      }
-}
-int main()
+
+    int main()
 {
 }
